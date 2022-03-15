5b17b0f75f9f4e08559e36c2430e3c3d4fb1e0a0
Matthieu Melin <matthieumelin62@gmail.com>
2022-03-15 15:18:00 +0100

    ajout de l'en-tête avec les données du photographe sur la page photographer.html

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -11,24 +11,35 @@ async function init() {
   // Fonction pour créer tous les éléments de la page
   async function createElements() {
     const header = document.querySelector(".photograph_header");
-    const headerContent = document.createElement("div");
+    const headerContent = document.querySelector(".photograph_header_content");
     if (header) {
+      const contactButton = document.querySelector(".contact_button");
       const fullName = document.createElement("h1");
       const localization = document.createElement("p");
+      const tagline = document.createElement("p");
+      const picture = document.createElement("img");
 
       // Ajout des classes personnalisées sur chaque élément
-      headerContent.classList.add("photograph_header_content");
       fullName.classList.add("photograph_header_content_name");
       localization.classList.add("photograph_header_content_location");
+      tagline.classList.add("photograph_header_content_tagline");
+      picture.classList.add("photograph_header_content_picture");
+
+      // Ajout de chaque attribut sur chaque élément
+      picture.setAttribute("src", `assets/photographers/${photograph.portrait}`);
+      picture.setAttribute("alt", photograph.name);
 
       // Ajout des textes sur chaques text
       fullName.textContent = photograph.name;
       localization.textContent = `${photograph.city}, ${photograph.country}`;
+      tagline.textContent = photograph.tagline;
 
       // Ajout des éléments au header
-      header.appendChild(headerContent);
+      headerContent.after(contactButton);
       headerContent.appendChild(fullName);
       headerContent.appendChild(localization);
+      headerContent.appendChild(tagline);
+      header.appendChild(picture);
     }
   }
 }
